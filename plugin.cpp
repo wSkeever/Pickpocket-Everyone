bacb2f73317d474e3afa37d95908f5c613434db0
wSkeever <theregisteredone@gmail.com>
2026-02-22 23:55:25 -0800

    Adding fix for activate scripts

--- plugin.cpp
+++ plugin.cpp
@@ -3,21 +3,67 @@
 using namespace RE;
 
 namespace PickpocketEveryone {
+    inline std::vector<RE::TESRace*> g_modifiedRaces;
+    using ActivateSignature = bool(TESObjectREFR*, TESObjectREFR*, std::uint8_t, TESBoundObject*, std::int32_t, bool);
+    REL::Relocation<ActivateSignature> activate_original;
+
     void SetAllRacesAllowPickpicket() {
         TESDataHandler* dataHandler = TESDataHandler::GetSingleton();
         if (dataHandler) {
             auto races = dataHandler->GetFormArray<TESRace>();
             for (auto* race : races) {
-                race->data.flags.set(RE::RACE_DATA::Flag::kAllowPickpocket);
+                if (race && !race->data.flags.all(RE::RACE_DATA::Flag::kAllowPickpocket)) {
+                    g_modifiedRaces.push_back(race);
+                    race->data.flags.set(RACE_DATA::Flag::kAllowPickpocket);
+                }
             }
         }
     }
 
+    TESRace* GetRefRace(TESObjectREFR* ref) {
+        if (!ref) {
+            return nullptr;
+        }
+        auto actor = ref->As<RE::Actor>();
+        if (!actor) {
+            return nullptr;
+        }
+        return actor->GetRace();
+    }
+
+    bool IsRaceModified(TESRace* race) {
+        if (!race) {
+            return false;
+        }
+        return std::find(g_modifiedRaces.begin(), g_modifiedRaces.end(), race) != g_modifiedRaces.end();
+    }
+
+    bool ActivateHook(TESObjectREFR* a_targetRef, TESObjectREFR* a_activatorRef, std::uint8_t a_arg3,
+                      TESBoundObject* a_object, std::int32_t a_targetCount, bool a_defaultProcessingOnly) {
+        auto targetRace = GetRefRace(a_targetRef);
+        if (IsRaceModified(targetRace)) {
+            targetRace->data.flags.reset(RACE_DATA::Flag::kAllowPickpocket);
+            return activate_original(a_targetRef, a_activatorRef, a_arg3, a_object, a_targetCount, a_defaultProcessingOnly);
+            targetRace->data.flags.set(RACE_DATA::Flag::kAllowPickpocket);
+        }
+        return activate_original(a_targetRef, a_activatorRef, a_arg3, a_object, a_targetCount, a_defaultProcessingOnly);
+    }
+
+    void InstallCanPickpocketHook() {
+        REL::RelocationID hook{55610, 56139};
+        ptrdiff_t offset = REL::Offset(0x4A).offset();
+        auto& trampoline = SKSE::GetTrampoline();
+        activate_original = trampoline.write_call<5>(hook.address() + offset, ActivateHook);
+    }
+
     SKSEPluginLoad(const SKSE::LoadInterface* skse) {
         SKSE::Init(skse);
+        SKSE::AllocTrampoline(14);
         SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
             if (message->type == SKSE::MessagingInterface::kDataLoaded) {
                 SetAllRacesAllowPickpicket();
+                
+                InstallCanPickpocketHook();
             }
         });
         return true;
